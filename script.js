8afecb9fb52e64267b43512cdf816af7981f60a5
Daniel Muñuico <daniel_munuico@cramerpuno.edu.pe>
2021-10-11 12:56:19 -0500

    first commit

---
+++ script.js
@@ -0,0 +1,45 @@
+let c1 = document.getElementById(c1);
+let c2 = document.getElementById(c2);
+let c3 = document.getElementById(c3);
+let c4 = document.getElementById(c4);
+let c5 = document.getElementById(c5);
+let c6 = document.getElementById(c6);
+let c7 = document.getElementById(c7);
+let c8 = document.getElementById(c8);
+let c9 = document.getElementById(c8);
+let play1 = document.getElementById(play1);
+let play2 = document.getElementById(play2);
+let play3 = document.getElementById(play3);
+let play4 = document.getElementById(play4);
+let play5 = document.getElementById(play5);
+let play6 = document.getElementById(play6);
+let play7 = document.getElementById(play7);
+let play8 = document.getElementById(play8);
+let play9 = document.getElementById(play9);
+play1.addEventListener("click", ()=>{
+    c1.play();
+});
+play2.addEventListener("click", ()=>{
+    c2.play();
+});
+play3.addEventListener("click", ()=>{
+    c3.play();
+});
+play4.addEventListener("click", ()=>{
+    c4.play();
+});
+play5.addEventListener("click", ()=>{
+    c5.play();
+});
+play6.addEventListener("click", ()=>{
+    c6.play();
+});
+play7.addEventListener("click", ()=>{
+    c7.play();
+});
+play8.addEventListener("click", ()=>{
+    c8.play();
+});
+play9.addEventListener("click", ()=>{
+    c9.play();
+});
